18e65baf701b4d7c0f78de41741767d95d079705
rauzola <raul_sigoli@hotmail.com>
2020-06-12 18:55:12 -0300

    26 - Navegando no Saga

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
 
+//import history from './services/history';
 import store from './store';
 
 function App() {
