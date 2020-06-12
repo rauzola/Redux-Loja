c05816e1b804aa49e8aa22fc2ed387142b6232a4
rauzola <raul_sigoli@hotmail.com>
2020-06-11 22:04:12 -0300

    09 - Buscando produtos da API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,46 @@
 import React, { Component } from 'react';
 import { MdShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../Util/format';
 import api from '../../services/api';
 
 import { ProductList } from './styles';
 
 export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
   render() {
+    const { products } = this.state;
+
     return (
       <ProductList>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x"
-            alt="Tenis"
-          />
-          <strong>Tênis muito legal</strong>
-          <span>R$129,90</span>,
-          <button type="button">
-            <div>
-              <MdShoppingCart size={16} color="fff" /> 3
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+
+            <button type="button">
+              <div>
+                <MdShoppingCart size={16} color="#FFF" />{' '}
+              </div>
+
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
       </ProductList>
     );
   }
